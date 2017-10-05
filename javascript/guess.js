b6950b5d3983a1de544ec350836ee0d762a00b5c
Jason <vivanaranja@gmail.com>
2017-10-05 16:18:42 -0700

    a few more issues solved and a few to go

--- javascript/guess.js
+++ javascript/guess.js
@@ -1,67 +1,78 @@
+/* ISSUES
+	guessesRemain does not reset. for loop with break for when/if correct answer is given
+	playerGuess should also clear at this point.
+	playerGuess does not append... DOM appendChild()?
+	I think I'm close here.
+*/
+
+
+
+
+
 //var for all letters in the alphabet
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 //var for wins, losses, remaining guesses
 
+
 var gameWins = 0;
 var gameLosses = 0;
 var guessesRemain = 10;
-var guessedLetters;
-
-
-//var for the letter guessed
-var playerGuess = document.getElementById('playerGuess');
-
+var guessedElem = [];
 
 //var for computer letter choice 
 //define length of alphabet
 var alphbetLength = alphabet.length;
-//choose a random letter from the length
-var cpuLetter = alphabet[Math.floor(Math.random()*alphabet.length)];
 
+//choose a random letter from the length
+var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+//show what letter was chosen by computer in console
 console.log(cpuLetter);
 
-
 //player guesses by pressing a key - onkeyup
-
 document.onkeyup = function(event) {
-var playerGuess = event.key;
 
-
-if(playerGuess === cpuLetter) {
-	gameWins++;
-} else {
-	guessesRemain--;
-}
-
-if(guessesRemain === 0) {
-	gameLosses++;
-}
+    //determine which key was chosen
+    var playerGuess = event.key;
+    guessedElem.innerHTML = playerGuess;
 
 
 
-//getElementbyID + innerHTML for html to show guesses/wins/losses/remainder
+    //apply game rules
+    if (playerGuess === cpuLetter) {
+        gameWins++;
 
-function event() {
-	}
+    } else {
+        guessesRemain--;
+    }
 
-document.getElementById('playerGuess').innerHTML = playerGuess;
-
-
-document.getElementById('gameWins').innerHTML = gameWins;
-
-document.getElementById('gameLosses').innerHTML = gameLosses;
-
-document.getElementById('guessesRemain').innerHTML = guessesRemain;
-
-document.getElementById('guessedLetters').innerHTML = guessesSoFar;
+    if (guessesRemain === 0) {
+        gameLosses++;
+    }
 
 
-// var alphbetLength = alphabet.length;
 
-// for (i = 0; i < alphabet.length; i++) {
+    //getElementbyID + innerHTML for html to show guesses/wins/losses/remainder
+
+    // function event() {}
+
+    // document.getElementById('playerGuess').innerHTML = playerGuess;
+
+
+    document.getElementById('gameWins').innerHTML = gameWins;
+
+    document.getElementById('gameLosses').innerHTML = gameLosses;
+
+    document.getElementById('guessesRemain').innerHTML = guessesRemain;
+
+    document.getElementById('guessedLetters').innerHTML = guessesSoFar;
+
+
+    // var alphbetLength = alphabet.length;
+
+    // for (i = 0; i < alphabet.length; i++) {
 
 };
-
