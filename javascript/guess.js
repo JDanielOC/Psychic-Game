2b3bdede0678479f090d4ab8372e19ca2be09c43
Jason <vivanaranja@gmail.com>
2017-10-08 07:29:15 -0700

    adding more game logic

--- javascript/guess.js
+++ javascript/guess.js
@@ -1,12 +1,12 @@
 /* ISSUES
-	guessesRemain does not reset. (if else analysis)  for loop with break for when/if correct answer is given
-	playerGuess should also clear at this point. FIXED: add multiple elses depending on if to effect game play (guessesRemain resets if game win AND if you run out of guesses.)
-	playerGuess does not append... DOM appendChild()? FIXED: .join(', ');
-    cpuLetter does not re-choose a random letter when game is won or lost. FIXED.
-	I think I'm close here.
-    One more issue: 
+     - guessesRemain does not reset. (if else analysis)  for loop with break for when/if correct answer is given
+    playerGuess should also clear at this point. FIXED: add multiple elses depending on if to effect game play (guessesRemain resets if game win AND if you run out of guesses.)
+     - playerGuess does not append... DOM appendChild()? FIXED: .join(', ');
+     - cpuLetter does not re-choose a random letter when game is won or lost. FIXED: tell the computer to choose a random letter in the if/else statements.
+    I think I'm close here.
+     - One more issue: 
     alert if key pressed is not in the alphabet array.
-    And another:
+     - And another:
     if gameWins is 5 reset game/if gameLosses is 5 reset game both with alerts. Seems to go to 5, require a key event and then alerts. need it to perform the alert before the key event.
 */
 
@@ -25,10 +25,13 @@ var guessedLetters = [];
 
 /*var for computer letter choice 
 choose a random letter from the length*/
-var alphabetLength = alphabet.length;
 
-var cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+var letterGetter = function() {
+    return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+var cpuLetter = letterGetter()
 //show what letter was chosen by computer in console
 console.log(cpuLetter);
 
@@ -38,37 +41,17 @@ document.onkeyup = function(event) {
     //determine which key was chosen
     var guess = event.key;
     //guessedLetters.innerHTML = guess;
-        guessedLetters.push(guess);
+    guessedLetters.push(guess);
 
-    //apply game rules
 
-    // if (gameWins === 5) {
-    //     alert("You Win! Success."),
-    //     gameWins = 0,
-    //     gameLosses = 0,
-    //     guessesRemain = 10, 
-    //     guessedLetters = [], 
-    //     cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
-    //     console.log(cpuLetter);
 
-    // }
-
-    // if (gameLosses === 5) {
-    //     alert("CPU Wins! Agony."),
-    //     gameWins = 0,
-    //     gameLosses = 0,
-    //     guessesRemain = 10, 
-    //     guessedLetters = [], 
-    //     cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
-    //     console.log(cpuLetter);
-
-    // }
+    //apply game logic ---found out order of operations is very important here. wins and losses would not work if they were placed before guess logic.
 
     if (guess === cpuLetter) {
-        gameWins++, 
-        guessesRemain = 10, 
-        guessedLetters = [], 
-        cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
+        gameWins++;
+        guessesRemain = 10;
+        guessedLetters = [];
+        cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
         console.log(cpuLetter);
 
     } else {
@@ -76,17 +59,34 @@ document.onkeyup = function(event) {
     }
 
     if (guessesRemain === 0) {
-        gameLosses++, 
+        gameLosses++;
+        guessesRemain = 10;
+        guessedLetters = [];
+        cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(cpuLetter);
+    }
+
+        if (gameWins === 5) {
+        alert("You Win! Success.");
+             gameWins = 0;
+            gameLosses = 0,
+            guessesRemain = 10, 
+            guessedLetters = [], 
+            cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
+            console.log(cpuLetter);
+
+    }
+
+    if (gameLosses === 5) {
+        alert("CPU Wins! The agony of defeat."),
+        gameWins = 0,
+        gameLosses = 0,
         guessesRemain = 10, 
         guessedLetters = [], 
         cpuLetter = alphabet[Math.floor(Math.random() * alphabet.length)]
         console.log(cpuLetter);
-    }
 
-    // if (guess !== alphabet[i]) {
-    //     alert("Invalid Entry. Try a letter.")
-    // }
-    
+    }
 
 
     //getElementbyID + innerHTML for html to show guesses/wins/losses/remainder
